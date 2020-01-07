7aedfd57030e1c1baf98ce4e867a2f71a44067f3
michleia <>
2020-01-07 12:59:01 +0800

    change mqtt to gomqtt

--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module github.com/michelia/umq
 
 go 1.13
 
-require (
-	github.com/eclipse/paho.mqtt.golang v1.2.1-0.20191115161417-939f51659320
-	github.com/gorilla/websocket v1.4.1 // indirect
-	github.com/michelia/ulog v1.0.6
-)
+require github.com/256dpi/gomqtt v0.14.1
--- umq.go
+++ umq.go
@@ -1,159 +1,129 @@
 package umq
 
 import (
-	"errors"
-	"fmt"
-	"sync"
+	"strings"
 	"time"
 
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/michelia/ulog"
+	mqc "github.com/256dpi/gomqtt/client"
+	"github.com/256dpi/gomqtt/packet"
 )
 
-type (
-	MessageHandler = mqtt.MessageHandler
-	Client         = mqtt.Client
-	Message        = mqtt.Message
-)
+// Match checks if the Topic Name matches the Topic Filter.
+// topicName   topic/foo
+// topicFilter topic/+
+func Match(topicName, topicFilter string) bool {
+	// Tokenize the Topic Name.
+	nameTokens := strings.Split(topicName, "/")
+	nameTokensLen := len(nameTokens)
 
-var ErrConnectTimeout = errors.New("mqc Connect token.WaitTimeout")
-var ErrPublishTimeout = errors.New("mqc Publish token.WaitTimeout")
+	// Tolenize the Topic Filter.
+	filterTokens := strings.Split(topicFilter, "/")
+
+	for i, t := range filterTokens {
+		switch t {
+		case "#":
+			return i != 0 || !strings.HasPrefix(nameTokens[0], "$")
+		case "+":
+			if i == 0 && strings.HasPrefix(nameTokens[0], "$") {
+				return false
+			}
+
+			if nameTokensLen <= i {
+				return false
+			}
+		default:
+			if nameTokensLen <= i || t != nameTokens[i] {
+				return false
+			}
+		}
+	}
+
+	return len(filterTokens) == nameTokensLen
+}
 
 // Config 构建mq需要的配置
 type Config struct {
-	BrokerAddr           string
-	UserName             string
-	Password             string
-	KeepAlive            int
-	DisableCleanSession  bool
-	ConnectTimeout       int // 单位ms
-	PublishTimeout       int // 单位ms
-	MaxReconnectInterval int
+	ClientID            string
+	BrokerUrl           string        // "tcp://user:password@host:port"
+	KeepAlive           time.Duration // 单位s 默认是30s
+	DisableCleanSession bool
+	PublishTimeout      time.Duration // 单位s 默认是1s
+	MaxReconnectDelay   time.Duration // 单位s 默认是10s
 }
 
-type FuncClientID func() string
+type Message = packet.Message
+type QOS = packet.QOS
 
-type subscribe struct {
-	topic    string
-	qos      byte
-	callback MessageHandler
+type MessageHandler func(topic string, payload []byte)
+
+type MQC struct {
+	*mqc.Service
+	cfg            *mqc.Config
+	Subscribes     map[string]int
+	PublishTimeout time.Duration
 }
 
-type MQ struct {
-	slog         ulog.Logger
-	mqc          mqtt.Client
-	mutex        sync.Mutex
-	Config       Config
-	funcClientID FuncClientID
-	subs         []subscribe
+func (m *MQC) Start() error {
+	m.Service.Start(m.cfg)
+	var err error
+	for topic, qos := range m.Subscribes {
+		err = m.Service.Subscribe(topic, QOS(qos)).Wait(5 * time.Second)
+	}
+	return err
+}
+
+func (m *MQC) Stop() {
+	m.Service.Stop(true)
+}
+func (m *MQC) Publish(msg *Message) error {
+	return m.Service.PublishMessage(msg).Wait(m.PublishTimeout)
+}
+
+// SetMessageCallback 在 Start 之前
+// func(msg *umq.Message) {
+// 	switch {
+// 	case umq.Match(msg.Topic, topicRespond):
+// 		go msgListen(msg.Topic, msg.Payload)
+// 	case umq.Match(msg.Topic, topicReport):
+// 		go armReportGz(msg.Topic, msg.Payload)
+// 	}
+// }
+func (m *MQC) SetMessageCallback(callback func(msg *Message)) {
+	m.Service.MessageCallback = func(msg *Message) error {
+		callback(msg)
+		return nil
+	}
 }
 
 /*
 New 创建操作MQ消息的对象
-
-slog: 带session的log
 config: 构建mq需要的配置
 */
-func New(slog ulog.Logger, config Config) *MQ {
-	if config.ConnectTimeout == 0 {
-		slog.Panic().Msg("ConnectTimeout except value > 0 ms")
+func New(config Config) *MQC {
+	cfg := mqc.NewConfig(config.BrokerUrl)
+	if config.KeepAlive != 0 {
+		cfg.KeepAlive = (time.Second * config.KeepAlive).String()
 	}
-	if config.PublishTimeout == 0 {
-		slog.Panic().Msg("PublishTimeout except value > 0 ms")
+	if cfg.ClientID != "" {
+		cfg.ClientID = config.ClientID
 	}
-	mq := MQ{
-		slog:   slog,
-		Config: config,
+	cfg.CleanSession = true
+	if config.DisableCleanSession {
+		cfg.CleanSession = false
 	}
-	return &mq
-}
+	s := mqc.NewService()
+	s.ResubscribeAllSubscriptions = true
+	if config.MaxReconnectDelay != 0 {
+		s.MaxReconnectDelay = time.Second * config.MaxReconnectDelay
+	}
 
-func (mq *MQ) SetClientID(funcClientID FuncClientID) {
-	mq.funcClientID = funcClientID
-}
-
-func (mq *MQ) AddSubscrAdd(topic string, qos int, callback MessageHandler) {
-	mq.subs = append(mq.subs, subscribe{
-		topic:    topic,
-		qos:      byte(qos),
-		callback: callback,
-	})
-}
-
-func (mq *MQ) newClient() mqtt.Client {
-	clientID := mq.funcClientID()
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(mq.Config.BrokerAddr)
-	opts.SetClientID(clientID) // 每次连接的时候都更换ClientID
-	opts.SetUsername(mq.Config.UserName)
-	opts.SetPassword(mq.Config.Password)
-	opts.SetProtocolVersion(3)
-	if mq.Config.KeepAlive != 0 {
-		opts.SetKeepAlive(time.Second * time.Duration(mq.Config.KeepAlive))
+	m := MQC{
+		cfg:            cfg,
+		Service:        s,
+		PublishTimeout: time.Second,
 	}
-	if mq.Config.DisableCleanSession == true {
-		opts.SetCleanSession(false) // 默认是 CleanSession 即不建队列, 不持久化信息
+	if config.PublishTimeout != 0 {
+		m.PublishTimeout = time.Second * config.PublishTimeout
 	}
-	// opts.SetConnectRetry(true)
-	opts.SetConnectTimeout(time.Millisecond * time.Duration(mq.Config.ConnectTimeout))
-	// opts.SetCleanSession(false) // 默认是清除session, 重连后Subscribe都失效了, 所以要设置为false, 自带的重连不需要设置这个参数
-	fmt.Println("ClientID: " + opts.ClientID)
-	opts.SetConnectionLostHandler(func(client mqtt.Client, err error) {
-		mq.slog.Warn().Str("clientID", clientID).Err(err).Msg("error disconnect EMQ")
-	})
-	opts.SetOnConnectHandler(func(cli mqtt.Client) {
-		// 添加订阅
-		for _, v := range mq.subs {
-			cli.Subscribe(v.topic, v.qos, v.callback)
-		}
-		mq.slog.Warn().Str("clientID", clientID).Msg("success connect EMQ and subscribe")
-	})
-	return mqtt.NewClient(opts)
-}
-
-// Connect 更换client 再次连接
-func (mq *MQ) Connect() error {
-	mq.mutex.Lock()
-	defer mq.mutex.Unlock()
-	if mq.mqc != nil && mq.mqc.IsConnected() {
-		mq.mqc.Disconnect(100)
-	}
-	mq.mqc = mq.newClient()
-	token := mq.mqc.Connect()
-	if !token.WaitTimeout(time.Millisecond * time.Duration(mq.Config.ConnectTimeout)) {
-		return ErrConnectTimeout
-	}
-	return token.Error()
-}
-
-// Publish 尝试发送三次
-func (mq *MQ) Publish(slog ulog.Logger, topic string, qos uint, retained bool, payload interface{}) error {
-	var err error
-	for i := 0; i < 3; i++ {
-		token := mq.mqc.Publish(topic, byte(qos), retained, payload)
-		if !token.WaitTimeout(time.Millisecond * time.Duration(mq.Config.PublishTimeout)) {
-			slog.Warn().Caller().Err(ErrPublishTimeout).Int("retry", i).Msg("重连后再Publish")
-			err = mq.Connect()
-			if err != nil {
-				slog.Error().Caller().Err(err).Int("retry", i).Msg("再次重连后再Publish")
-			}
-			continue
-		}
-		switch err = token.Error(); err {
-		case nil:
-			slog.Info().Int("retry", i).Msg("已下发")
-			return nil
-		case mqtt.ErrNotConnected:
-			err = mq.Connect()
-			if err != nil {
-				slog.Error().Caller().Err(err).Int("retry", i).Msg("没有连接,重连后再Publish")
-			}
-			continue
-		default:
-			slog.Error().Caller().Err(err).Int("retry", i).Msg("mq.mqc.Publish other error")
-			continue
-			// return err
-		}
-	}
-	return err
+	return &m
 }
